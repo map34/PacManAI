4b7025a28f67c1bd8d4fe08ad9da15b9213562ff
Adrian Prananda <mochamad.prananda@yahoo.com>
2015-05-28 14:04:48 -0700

    Added maze to list

--- maze.py
+++ maze.py
@@ -1,16 +1,11 @@
-maze = [["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-		["O", "O", "O", "O", "O", "O", "O", "O", "O"]]
+def getMaze():
+    mazeList = []
+    fname = "maze.txt"
+    with open(fname) as f:
+        content = [x.strip('\n') for x in f.readlines()]
+        del content[-1]
+        for wall in content:
+            mazeList.append(list(wall))
+    return mazeList
+
+getMaze()
