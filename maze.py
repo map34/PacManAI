faa0a246a5a99058330417bfcf6e842a60fd45f1
Adrian Prananda <mochamad.prananda@yahoo.com>
2015-05-28 15:04:12 -0700

    Sexy maze with gui

--- maze.py
+++ maze.py
@@ -1,3 +1,6 @@
+from tkinter import *
+
+
 def getMaze():
     mazeList = []
     fname = "maze.txt"
@@ -8,4 +11,35 @@ def getMaze():
             mazeList.append(list(wall))
     return mazeList
 
-getMaze()
+def dim(mazeList):
+    return [len(mazeList), len(mazeList[0])]
+
+def createGUI():
+
+    mazeList = getMaze()
+    print(dim(mazeList))
+
+    master = Tk()
+
+    w = Canvas(master, width=630, height=630)
+    w.pack()
+
+    x0 = 0
+    y0 = 0
+    x1 = 30
+    y1 = 30
+    for i in range(21):
+        x0 = 0
+        x1 = 30
+        for j in range (21):
+            if mazeList[i][j] == '1':
+                w.create_rectangle(x0, y0, x1,y1, fill="blue")
+            x0 += 30
+            x1 += 30
+        y0 += 30
+        y1 += 30
+
+    mainloop()
+
+createGUI()
+
